918e278a0e66ef2d0b932c99a8406217ff233661
Samuel Luis Mendoza <sluismen@emeal.nttdata.com>
2022-04-07 15:28:49 -0500

    Init

---
+++ src/main/java/com/bootcamp/msclients/MsClientsApplication.java
@@ -0,0 +1,13 @@
+package com.bootcamp.msclients;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsClientsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsClientsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bootcamp/msclients/MsClientsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bootcamp.msclients;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsClientsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
